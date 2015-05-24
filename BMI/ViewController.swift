ed95a49bab3f5c3cbb96fd930c8d791027a0e623
Sanjeev S Nair <sanjeevsureshn@gmail.com>
2015-05-24 22:47:29 +0300

    initial commit

--- BMI/ViewController.swift
+++ BMI/ViewController.swift
@@ -8,8 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
+    
+    var height:Double?
+    var weight:Double?
+    var bmi:Double? {
+        get {
+            if height != nil && weight != nil {
+                return weight!/(height! * height!)
+            } else {
+                return nil
+            }
+        }
+    }
 
+    @IBOutlet weak var ans: UILabel!
+    @IBOutlet weak var heightTextField: UITextField!
+    @IBOutlet weak var weightTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +34,37 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func updateUI() {
+        if let b = self.bmi {
+            self.ans.text = String(format: "%4.1f", b)
+        }
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        switch(textField) {
+        case self.weightTextField:
+            //TODO:set weight
+            let s = textField.text
+            let dv = NSNumberFormatter().numberFromString(s)?.doubleValue
+            self.weight = dv
+            
+        case self.heightTextField:
+            //TODO:set height
+            let s = textField.text
+            let dv = NSNumberFormatter().numberFromString(s)?.doubleValue
+            self.height = dv
+        default:
+            println("Something gone wrong!");
+            
+        }
     }
 
 
